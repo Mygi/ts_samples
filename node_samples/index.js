681d9306e2d0b83bb2304b801f64d294e0fff2f8
Andrew Glenn <andrew.glenn@orco.com.au>
2025-03-18 05:57:33 +1100

    compile and run commands and configuration

--- node_samples/index.js
+++ node_samples/index.js
@@ -1,29 +1,24 @@
-// Entry point to test samples.
-//  `node index.js` after transpiling
-
 import * as readline from 'readline/promises';
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 try {
     // NOTE: the second parameter (the timeout) is optional.
     const answer = await rl.question('Run tester? [y/n] ', {
         signal: AbortSignal.timeout(10_000) // 10s timeout
     });
-
-    switch(answer.toLowerCase()) {
-    case 'y':
-        console.log('Super!');
-        break;
-    case 'n':
-        console.log('Sorry! :(');
-        break;
-    default:
-        console.log('Invalid answer!');
+    switch (answer.toLowerCase()) {
+        case 'y':
+            console.log('Super!');
+            break;
+        case 'n':
+            console.log('Sorry! :(');
+            break;
+        default:
+            console.log('Invalid answer!');
     }
-} finally {
+}
+finally {
     rl.close();
 }
